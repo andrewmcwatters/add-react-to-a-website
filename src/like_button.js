bbc21ecfa2dc81889eac78e8712ab5bdfb8933c3
Andrew McWatters <andrew@andrewmcwatters.com>
2025-10-23 14:57:17 -0700

    Create like_button.js

---
+++ src/like_button.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { useState } = React;
+
+const e = React.createElement;
+
+function LikeButton() {
+  const [liked, setLiked] = useState(false);
+
+  if (liked) {
+    return 'You liked this.';
+  }
+
+  return (
+    <button onClick={() => setLiked(true)}>
+      Like
+    </button>
+  );
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
